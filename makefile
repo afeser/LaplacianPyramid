73d1607e0c25da58eb043f989fd39c6563811b98
Fatih Eser <afeser@outlook.com.tr>
2018-09-14 15:26:49 +0300

    Add new options into main.

--- makefile
+++ makefile
@@ -5,7 +5,7 @@ LIBRARY_PATH = "/home/afeser/Dropbox/Documents/Canavarr/Pratik/MyLibrary/lib"
 INCLUDE_PATH = "/home/afeser/Dropbox/Documents/Canavarr/Pratik/MyLibrary/include"
 
 LaplacianPyramid: createDirs
-	$(CC) -g -o $(BIN_DIR)/main.bin -I$(INCLUDE_PATH) $(SRC_DIR)/main.cu -lcudart -lcuda -lppm -L$(LIBRARY_PATH) #-L/usr/local/cuda-9.2/lib64/ #kutuphaneyi de nvcc ile yaptigimda -lcudart ve -lcuda olmadiginda da hata verdi
+	$(CC) -g -o $(BIN_DIR)/main.bin -I$(INCLUDE_PATH) $(SRC_DIR)/main.cu -lcudart -lcuda -lppm -lpyramid -L$(LIBRARY_PATH) #-L/usr/local/cuda-9.2/lib64/ #kutuphaneyi de nvcc ile yaptigimda -lcudart ve -lcuda olmadiginda da hata verdi
 
 
 
--- src/main.cu
+++ src/main.cu
@@ -1,12 +1,77 @@
 #include "LaplacianPyramid.cu"
+#include <string.h>
+#include "pyramid.hpp"
 
-int main(void){
-  char inFileElma[] = "data/1600.ppm";
-  char outFile[]    = "output/LaplacianL1.ppm";
-  Picture in = Picture(inFileElma, true);
+void help(){
+  printf("Use : \n");
+  printf("command sharpen number_of_additions\n");
+  printf("command sharpen input_name output_name number_of_additions\n\n");
 
-  Picture yukari1 = yukari(in);
-
-  (*yukari1.getLaplacian()).write(outFile);
+  printf("command pyramid laplacian height\n");
+  printf("command pyramid gaussian height\n");
 }
 
+int main(int argc, char *argv[]){
+  if(argc<2){
+    help();
+    return 0;
+  }
+
+  if(!strcmp(argv[1], "sharpen")){
+    char inFileElma[]   = "data/Sample.ppm";
+    char outFile[]      = "output/SampleSharpened.ppm";
+    char inputPath[50]  = "data/";
+    char outputPath[50] = "output/";
+
+    if(argc == 3){
+      printf("Input  = %s\n", inFileElma);
+      printf("Output = %s\n", outFile);
+
+      sharpenEdges(inFileElma, outFile, atoi(argv[2]));
+    }else if(argc == 5){
+      printf("Input  = %s\n", strcat(inputPath, argv[2]));
+      printf("Output = %s\n", strcat(outputPath, argv[3]));
+
+      sharpenEdges(inputPath, outputPath, atoi(argv[4]));
+    }else{
+      help();
+      return 0;
+    }
+  }
+
+  if(!strcmp(argv[1], "pyramid")){
+    char inFileElma[]    = "data/Sample.ppm";
+    char outFile[]       = "output/SampleLayer";
+    char outFileTemp[50];
+
+    Picture in = Picture(inFileElma, true);
+
+    int height = atoi(argv[3]);
+
+    if(!strcmp(argv[2], "laplacian")){
+      Pyramid p;
+      p.createLaplacian(in, height);
+
+      for(int i = 0; i<height; i++){
+        // C tarzi oldu malesef
+        sprintf(outFileTemp, "%s%d.ppm", outFile, i);
+
+        p.getLayer(i).write(outFileTemp);
+      }
+    }else if(!strcmp(argv[2], "gaussian")){
+        Pyramid p;
+        p.createGaussian(in, height);
+
+        for(int i = 0; i<height; i++){
+          // C tarzi oldu malesef
+          sprintf(outFileTemp, "%s%d.ppm", outFile, i);
+
+          p.getLayer(i).write(outFileTemp);
+        }
+    }else{
+      help();
+      return 0;
+    }
+  }
+
+}
